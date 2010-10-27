3ae9c5f766dd16c1809d6b09e0037898c60eebec
soft@byjean.eu <soft@byjean.eu>
2010-10-27 17:38:16 +0200

    kata_start ready

---
+++ src/test/dojo/kata/TestEncode.java
@@ -0,0 +1,14 @@
+package dojo.kata;
+
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.experimental.theories.suppliers.TestedOn;
+
+
+public class TestEncode{
+	@Test public void testtruth(){
+		assertTrue(true);
+	}
+}
