091d4a4a972bde28d7a22ae87ddba0113dfe8f47
soft@byjean.eu <soft@byjean.eu>
2010-10-27 17:38:46 +0200

    cleanup

--- src/test/dojo/kata/TestEncode.java
+++ src/test/dojo/kata/TestEncode.java
@@ -1,10 +1,9 @@
 package dojo.kata;
 
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
-import org.junit.experimental.theories.suppliers.TestedOn;
 
 
 public class TestEncode{
