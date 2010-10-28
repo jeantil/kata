0bf0962b135a752eee0510773e691e95cb92d0dc
Jean Helou <soft@byjean.eu>
2010-10-28 22:36:04 +0200

    Ok that's one nailed. Now we still have the second level of duplication to refactor away. we all agree that string==string.charAt(0) when length==1 thus we actually do have complete duplication.

--- src/main/dojo/kata/Encode.java
+++ src/main/dojo/kata/Encode.java
@@ -2,7 +2,7 @@ package dojo.kata;
 public class Encode {
 	public static String encode(String string) {
 		if(string.length()==1) return "1"+string;
-		if(string.length()==2) return "2"+string;
+		if(string.length()==2) return "2"+string.charAt(0);
 		return "";		
 	}
 }
