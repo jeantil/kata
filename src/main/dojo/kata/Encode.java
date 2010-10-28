f1fec5985d81837eb071cb7ff21d74c156b284eb
Jean Helou <soft@byjean.eu>
2010-10-28 22:38:02 +0200

    I'll be damned, I broke my tests again with my refactor lucky that this is an easy fix.

--- src/main/dojo/kata/Encode.java
+++ src/main/dojo/kata/Encode.java
@@ -1,8 +1,7 @@
 package dojo.kata;
 public class Encode {
 	public static String encode(String string) {
-		if(string.length()==1) return "1"+string;
-		if(string.length()==2) return "2"+string.charAt(0);
-		return "";		
+		return ""+string.length()+string.charAt(0);
+		//return "";		
 	}
 }
