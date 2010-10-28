2b025679f2a431b28010a907f914166dd827a8c9
Jean Helou <soft@byjean.eu>
2010-10-28 23:00:24 +0200

    All tests are green, we can handle any string composed of one or more occurence of the same character

--- src/main/dojo/kata/Encode.java
+++ src/main/dojo/kata/Encode.java
@@ -1,7 +1,7 @@
 package dojo.kata;
 public class Encode {
 	public static String encode(String string) {
+		if(string.length()==0) return "";
 		return ""+string.length()+string.charAt(0);
-		//return "";		
 	}
 }
