d2a70a025c90a08c89e280f6d9f9d7a9181e8ad6
Jean Helou <soft@byjean.eu>
2010-10-28 22:27:52 +0200

    Now I am not sure this step is really necessary but I decided to be as tedious as I can possibly be so I still go at it, introducing a new fake to make the test pass. We are back from 2 fakes to 3 fakes.

--- src/main/dojo/kata/Encode.java
+++ src/main/dojo/kata/Encode.java
@@ -2,6 +2,7 @@ package dojo.kata;
 public class Encode {
 	public static String encode(String string) {
 		if(string.length()==1) return "1"+string;
+		if(string.length()==2) return "2C";
 		return "";		
 	}
 }
