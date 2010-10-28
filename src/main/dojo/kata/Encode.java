5dab073389f5346d65548cb5640558aaabcb3159
Jean Helou <soft@byjean.eu>
2010-10-28 22:21:17 +0200

    Now the tests are still green, and the duplication really can't be missed

--- src/main/dojo/kata/Encode.java
+++ src/main/dojo/kata/Encode.java
@@ -1,8 +1,8 @@
 package dojo.kata;
 public class Encode {
 	public static String encode(String string) {
+		if(string.equals("A")) return "1A";
 		if(string.equals("B")) return "1B";
-		if(string.length()==0) return "";
-		return "1A";
+		return "";		
 	}
 }
