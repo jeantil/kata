adf215d59391e2f2d3647619c0c7c9f7bbcd5443
Jean Helou <soft@byjean.eu>
2010-10-28 22:32:00 +0200

    The tests pass, but we have a lot of duplication in the code ! Time to refactor.

--- src/main/dojo/kata/Encode.java
+++ src/main/dojo/kata/Encode.java
@@ -2,7 +2,10 @@ package dojo.kata;
 public class Encode {
 	public static String encode(String string) {
 		if(string.length()==1) return "1"+string;
-		if(string.length()==2) return "2C";
+		if(string.length()==2){
+			if(string.equals("CC"))	return "2C";
+			if(string.equals("DD"))	return "2D";
+		}
 		return "";		
 	}
 }
