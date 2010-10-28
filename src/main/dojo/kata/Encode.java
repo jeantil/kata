1f06f059a528c70ffd76acd13d54d19e4b93d1bf
Jean Helou <soft@byjean.eu>
2010-10-28 22:20:25 +0200

    The test pass, there are 5 fakes in this code, and there is duplication. The trick is that the duplication is not necessarily obvious let's refactor to make it really really obvious.

--- src/main/dojo/kata/Encode.java
+++ src/main/dojo/kata/Encode.java
@@ -1,6 +1,7 @@
 package dojo.kata;
 public class Encode {
 	public static String encode(String string) {
+		if(string.equals("B")) return "1B";
 		if(string.length()==0) return "";
 		return "1A";
 	}
