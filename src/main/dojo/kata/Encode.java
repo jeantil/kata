a0fb192dcb9c27e10a9c81fe15ea5de827b461fd
Jean Helou <soft@byjean.eu>
2010-10-28 22:33:28 +0200

    First attempt at a refactoring ... wait my tests are still red ! What did I miss ?

--- src/main/dojo/kata/Encode.java
+++ src/main/dojo/kata/Encode.java
@@ -2,10 +2,7 @@ package dojo.kata;
 public class Encode {
 	public static String encode(String string) {
 		if(string.length()==1) return "1"+string;
-		if(string.length()==2){
-			if(string.equals("CC"))	return "2C";
-			if(string.equals("DD"))	return "2D";
-		}
+		if(string.length()==2) return "2"+string;
 		return "";		
 	}
 }
