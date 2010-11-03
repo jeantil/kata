6341c1e3fbee9e4241a36c8af5b5f354b663ee1b
soft@byjean.eu <soft@byjean.eu>
2010-11-03 19:17:36 +0100

    add a simple answer to the failing test

--- src/main/dojo/kata/Encode.java
+++ src/main/dojo/kata/Encode.java
@@ -5,7 +5,8 @@ public class Encode {
 		return ""+string.length()+string.charAt(0);
 	}
 	public static String getFirstRun(String string) {
-		if(string.length()==0) return "";		
+		if(string.length()==0) return "";
+		if(string.length()>=2 && string.charAt(0)==string.charAt(1))return string.substring(0,2);
 		return string.substring(0,1);
 	}
 }
