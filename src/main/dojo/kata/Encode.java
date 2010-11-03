d61ddf11c0831bae140232a710ab2b6f9f1e2457
soft@byjean.eu <soft@byjean.eu>
2010-11-03 19:19:58 +0100

    Now all our tests pass, but we have some duplication, time for some refactoring.

--- src/main/dojo/kata/Encode.java
+++ src/main/dojo/kata/Encode.java
@@ -6,6 +6,7 @@ public class Encode {
 	}
 	public static String getFirstRun(String string) {
 		if(string.length()==0) return "";
+		if(string.length()>=3 && string.charAt(0)==string.charAt(1) && string.charAt(0)==string.charAt(2))return string.substring(0,3);
 		if(string.length()>=2 && string.charAt(0)==string.charAt(1))return string.substring(0,2);
 		return string.substring(0,1);
 	}
