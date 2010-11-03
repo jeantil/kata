95b0cfc326bbe353d6df9a4d27e9ffb27220aed7
soft@byjean.eu <soft@byjean.eu>
2010-11-03 19:18:40 +0100

    Add a failing test to make the duplication obvious

--- src/test/dojo/kata/TestEncode.java
+++ src/test/dojo/kata/TestEncode.java
@@ -51,5 +51,8 @@ public class TestEncode{
 	@Test public void getFirstRunAAB(){
 		assertTrue(getFirstRun("AAB").equals("AA"));
 	}
-
+	
+	@Test public void getFirstRunAAAB(){
+		assertTrue(getFirstRun("AAAB").equals("AAA"));
+	}
 }
