4ce6c441a3d6e5dabf0088b14d9165fd5f76e2ca
Jean Helou <soft@byjean.eu>
2011-02-27 22:39:30 +0100

    kata setup for ruby 1.8

---
+++ test.rb
@@ -0,0 +1,14 @@
+require 'test/unit'
+#if you have gems you can use the following
+#require 'rubygems' 
+#require 'redgreen' # gem install redgreen
+
+# Kata code goes after this line
+
+
+# Kata tests go after this line
+class TestWhatever < Test::Unit::TestCase
+  def test_truth
+    assert_equal(true, true)
+  end
+end
