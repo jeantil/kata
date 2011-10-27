691ec87a72f2f864155b8f3e724db3b282e09c82
jean helou <jean.helou@gmail.com>
2011-10-27 08:38:39 +0200

    PSUG Kata

---
+++ src/MastermindSpec.scala
@@ -0,0 +1,20 @@
+package eu.byjean
+
+import org.scalatest.FlatSpec
+import org.scalatest.matchers.ShouldMatchers
+import scala.None
+
+/**
+ *
+*/
+
+class MastermindSpecDone extends FlatSpec with ShouldMatchers{
+  behavior of "Mastermind"
+
+  it should "be true" in{
+    true should equal(true)
+  }
+
+
+
+}
